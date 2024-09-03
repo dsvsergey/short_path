8e3a4fd559f43a1e2e4ca23aa91b0e0c1fa9b94a
Serhii Ddobrovolskyi <serhii.dobrovolskyi@dipdev.studio>
2024-09-04 02:32:42 +0300

    Fix App

--- lib/presentation/pages/process_screen.dart
+++ lib/presentation/pages/process_screen.dart
@@ -8,58 +8,65 @@ class ProcessScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Process screen'),
-      ),
-      body: BlocBuilder<PathFinderCubit, PathFinderState>(
-        builder: (context, state) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Spacer(),
-                Text(
-                  state.isCalculationFinished
-                      ? 'All calculations have finished, you can send your results to server'
-                      : 'Calculating...',
-                  textAlign: TextAlign.center,
-                  maxLines: null,
-                  softWrap: true,
-                ),
-                const SizedBox(height: 16),
-                Text('${(state.progress * 100).toStringAsFixed(0)}%'),
-                const SizedBox(height: 16),
-                SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: CircularProgressIndicator(
-                    value: state.progress,
+    return BlocListener<PathFinderCubit, PathFinderState>(
+      listenWhen: (previous, current) =>
+          !previous.isResultsSent && current.isResultsSent,
+      listener: (context, state) {
+        Navigator.of(context).pushReplacementNamed('/result_list');
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Process screen'),
+        ),
+        body: BlocBuilder<PathFinderCubit, PathFinderState>(
+          builder: (context, state) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Spacer(),
+                  Text(
+                    state.isCalculationFinished
+                        ? 'All calculations have finished, you can send your results to server'
+                        : 'Calculating...',
+                    textAlign: TextAlign.center,
+                    maxLines: null,
+                    softWrap: true,
                   ),
-                ),
-                const SizedBox(height: 16),
-                Text(
-                    'Tasks completed: ${state.results.length}/${state.tasks.length}'),
-                if (state.isCalculationFinished && !state.isResultsSent) ...[
-                  const Spacer(),
-                  ElevatedButton(
-                    child: const Text('Send results to server'),
-                    onPressed: () {
-                      context.read<PathFinderCubit>().sendResultsToServer();
-                    },
-                  )
+                  const SizedBox(height: 16),
+                  Text('${(state.progress * 100).toStringAsFixed(0)}%'),
+                  const SizedBox(height: 16),
+                  SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: CircularProgressIndicator(
+                      value: state.progress,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                      'Tasks completed: ${state.results.length}/${state.tasks.length}'),
+                  if (state.isCalculationFinished && !state.isResultsSent) ...[
+                    const Spacer(),
+                    ElevatedButton(
+                      child: const Text('Send results to server'),
+                      onPressed: () {
+                        context.read<PathFinderCubit>().sendResultsToServer();
+                      },
+                    )
+                  ],
+                  if (state.isResultsSent) ...[
+                    const Spacer(),
+                    const Text('Results sent successfully!'),
+                  ],
+                  if (state.error != null)
+                    Text('Error: ${state.error}',
+                        style: const TextStyle(color: Colors.red)),
                 ],
-                if (state.isResultsSent) ...[
-                  const Spacer(),
-                  const Text('Results sent successfully!'),
-                ],
-                if (state.error != null)
-                  Text('Error: ${state.error}',
-                      style: const TextStyle(color: Colors.red)),
-              ],
-            ),
-          );
-        },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
